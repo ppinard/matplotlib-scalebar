5779d0a1e4a9381c4d3339efded79b26808dcce8
Philippe Pinard <philippe.pinard@gmail.com>
2017-06-03 19:01:09 +0100

    Fix typo

--- doc/example2.py
+++ doc/example2.py
@@ -4,6 +4,6 @@ from matplotlib_scalebar.scalebar import ScaleBar, IMPERIAL_LENGTH
 plt.figure()
 image = plt.imread(cbook.get_sample_data('grace_hopper.png'))
 plt.imshow(image)
-scalebar = ScaleBar(0.02, 'ft', IMPERIAL_LENGTH) # 1 pixel = 0.2 feet
+scalebar = ScaleBar(0.02, 'ft', IMPERIAL_LENGTH) # 1 pixel = 0.02 feet
 plt.gca().add_artist(scalebar)
 plt.savefig('example2.png')
