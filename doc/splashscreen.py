2f2f8b2c27637a23c4ea0fd437ba56f213d93e4e
Philippe Pinard <philippe.pinard@gmail.com>
2021-11-27 15:38:58 +0000

    Fix link

--- doc/splashscreen.py
+++ doc/splashscreen.py
@@ -4,9 +4,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-r = requests.get(
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Misc_pollen.jpg/315px-Misc_pollen.jpg"
-)
+r = requests.get("https://upload.wikimedia.org/wikipedia/commons/a/a4/Misc_pollen.jpg")
 im = Image.open(BytesIO(r.content))
 
 fig = plt.figure(figsize=(4, 4 / 1.3125))
@@ -16,7 +14,7 @@ ax.imshow(im, "gray")
 
 # According to Wikipedia, "the bean shaped grain in the bottom left corner is about 50 μm long."
 scalebar = ScaleBar(
-    50 / 37, "um", location="lower right", width_fraction=0.02, border_pad=1, pad=0.5
+    50 / 144, "um", location="lower right", width_fraction=0.02, border_pad=1, pad=0.5
 )
 ax.add_artist(scalebar)
 
